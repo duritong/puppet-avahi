d080689df1831b9b80fa0fdb26267f9f8210dc94
mh <mh@immerda.ch>
2009-11-14 13:17:45 +0100

    update file path to 0.25.x style

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,7 +20,7 @@ class avahi::base {
         ensure => present,
     }
     file{'/etc/init.d/avahi-daemon':
-        source => "puppet://${server}/avahi/init.d/${operatingsystem}/avahi-daemon",
+        source => "puppet://${server}/modules/avahi/init.d/${operatingsystem}/avahi-daemon",
         require => Package['avahi'],
         before => Service['avahi-daemon'],
         owner => root, group => 0, mode => 0755;
