b1c770fe11b5b52b5a3abfbf86b1a04e8946cdc8
mh <mh@immerda.ch>
2011-05-26 22:52:40 +0200

    cleanup module, do selinux type on selinux based hosts

---
+++ manifests/centos.pp
@@ -0,0 +1,15 @@
+class avahi::centos inherits avahi::base {
+
+  file{'/etc/init.d/avahi-daemon':
+    source => "puppet://${server}/modules/avahi/init.d/${operatingsystem}/avahi-daemon",
+    require => Package['avahi'],
+    before => Service['avahi-daemon'],
+    owner => root, group => 0, mode => 0755;
+  }
+
+  if $selinux == 'true' {
+    File['/etc/init.d/avahi-daemon']{
+      seltype => 'avahi_initrc_exec_t',
+    }
+  }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -10,25 +10,9 @@
 # General Public License version 3 as published by 
 # the Free Software Foundation.
 #
-
 class avahi {
-    include avahi::base
-}
-
-class avahi::base {
-    package{'avahi':
-        ensure => present,
-    }
-    file{'/etc/init.d/avahi-daemon':
-        source => "puppet://${server}/modules/avahi/init.d/${operatingsystem}/avahi-daemon",
-        require => Package['avahi'],
-        before => Service['avahi-daemon'],
-        owner => root, group => 0, mode => 0755;
-    }
-    service{'avahi-daemon':
-        ensure => running,
-        enable => true,
-        hasstatus => true,
-        require => Package[avahi],
-    }
+  case $operatingsystem {
+    centos: { include avahi::centos }
+    default: { include avahi::base }
+  }
 }
