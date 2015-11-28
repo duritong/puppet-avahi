2d8e142a18d4151857c2dbe267a3cfce1322e3f9
mh <mh@immerda.ch>
2015-11-28 17:41:09 +0100

    linting for future parser

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,13 +1,16 @@
+# manage avahi on centos
 class avahi::centos inherits avahi::base {
-  if $::lsbmajdistrelease == '5' {
+  if $::operatingsystemmajrelease == '5' {
     file{'/etc/init.d/avahi-daemon':
-      source => "puppet:///modules/avahi/init.d/${::operatingsystem}/avahi-daemon",
+      source  => "puppet:///modules/avahi/init.d/${::operatingsystem}/avahi-daemon",
       require => Package['avahi'],
-      before => Service['avahi-daemon'],
-      owner => root, group => 0, mode => 0755;
+      before  => Service['avahi-daemon'],
+      owner   => root,
+      group   => 0,
+      mode    => '0755';
     }
 
-    if $::selinux == 'true' {
+    if str2bool($::selinux) {
       File['/etc/init.d/avahi-daemon']{
         seltype => 'avahi_initrc_exec_t',
       }
