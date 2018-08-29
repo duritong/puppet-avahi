c41487cae1cdf1e18a700648276c9d333ca9cae0
mh <mh@immerda.ch>
2018-08-29 18:55:46 +0200

    fix location of file

--- manifests/disable.pp
+++ manifests/disable.pp
@@ -1,12 +1,4 @@
 # manifests/disable.pp
-
 class avahi::disable inherits avahi {
     include avahi::base::disable
-}
-
-class avahi::base::disable inherits avahi::base {
-    Service['avahi-daemon']{
-        ensure => stopped,
-        enable => false,
-    }
 }
